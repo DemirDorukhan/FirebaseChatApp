1959e34437d22bc8b5cad4d53430c955079a7cfe
Dorukhan Demir <cryptodorko@gmail.com>
2023-04-12 07:26:29 +0300

    Initial Commit

---
+++ whatzapp/whatzappApp.swift
@@ -0,0 +1,17 @@
+//
+//  whatzappApp.swift
+//  whatzapp
+//
+//  Created by Dorukhan Demir on 12.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct whatzappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
